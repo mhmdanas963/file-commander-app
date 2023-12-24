02b614e229cfeaa0616ff014f5a44676929c097b
Mohammed Anas <mohammedanasdk963@gmail.com>
2023-12-24 15:26:45 +0530

    Implement The Create File Command Feature

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const fsPromise = require("fs/promises");
 
 (async () => {
+  // Creates the file in the specified path
+  const createFile = async (path) => {
+    let newFileHandler;
+
+    try {
+      // Checks is the file already exists, if not it throws an error and catch block executes
+      newFileHandler = await fsPromise.open(path, "r");
+      // Exit the function is file exists
+      console.log(`The file ${path} already exists`);
+    } catch (e) {
+      // Creating the new file
+      newFileHandler = await fsPromise.open(path, "w");
+      console.log("File created successfully");
+    } finally {
+      // Close the opened file
+      newFileHandler.close();
+    }
+  };
+
+  const CREATE_FILE = "create a file";
+
   // Open the file to read data from it
   const commandFileHandler = await fsPromise.open("./command.txt", "r");
 
+  // change event
   commandFileHandler.on("change", async () => {
     // Size of the file
     const fileSize = (await commandFileHandler.stat()).size;
@@ -15,14 +37,19 @@ const fsPromise = require("fs/promises");
     const position = 0;
 
     // Reading data from the file
-    const fileData = await commandFileHandler.read(
-      buff,
-      offset,
-      length,
-      position
-    );
+    await commandFileHandler.read(buff, offset, length, position);
 
-    console.log(fileData.buffer.toString("utf-8"));
+    // Decoding the data
+    const command = buff.toString("utf-8");
+
+    // Checking the command
+    if (command.includes(CREATE_FILE)) {
+      // Gets the path from the command
+      const filePath = command.substring(CREATE_FILE.length + 1);
+
+      // Calling createFile function
+      await createFile(filePath);
+    }
   });
 
   // Watch the file "command.txt"
@@ -30,6 +57,7 @@ const fsPromise = require("fs/promises");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
+      // emitting change event
       commandFileHandler.emit("change");
     } else {
       break;
@@ -37,6 +65,4 @@ const fsPromise = require("fs/promises");
   }
 
   await commandFileHandler.close();
-
-  commandFileHandler.close();
 })();
