64fd75b8b852106c0e3555f676d7a18903589643
Mohammed Anas <mohammedanasdk963@gmail.com>
2023-12-24 21:28:37 +0530

    Implement The Delete File Command, Rename File Command, Add Content Command Features

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const fsPromise = require("fs/promises");
 
 (async () => {
+  const CREATE_FILE = "create the file";
+  const DELETE_FILE = "delete the file";
+  const RENAME_FILE = "rename the file";
+  const ADD_CONTENT = "add content to the file";
+
   // Creates the file in the specified path
-  const createFile = async (path) => {
+  const createFile = async (filePath) => {
     let newFileHandler;
 
     try {
       // Checks is the file already exists, if not it throws an error and catch block executes
-      newFileHandler = await fsPromise.open(path, "r");
+      newFileHandler = await fsPromise.open(filePath, "r");
       // Exit the function is file exists
-      console.log(`The file ${path} already exists`);
+      console.log(`The file ${filePath} already exists`);
     } catch (e) {
       // Creating the new file
-      newFileHandler = await fsPromise.open(path, "w");
+      newFileHandler = await fsPromise.open(filePath, "w");
       console.log("File created successfully");
     } finally {
       // Close the opened file
@@ -20,7 +25,48 @@ const fsPromise = require("fs/promises");
     }
   };
 
-  const CREATE_FILE = "create a file";
+  // Deletes the file specified in the path
+  const deleteFile = async (filePath) => {
+    try {
+      const deleted = await fsPromise.unlink(filePath);
+      if (deleted === undefined) {
+        console.log("File was deleted successfully");
+      } else {
+        throw new Error("Error in deleting the file");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Rename the file specified in the path to the name specified in the new path
+  const renameFile = async (filePath, newFilePath) => {
+    try {
+      const renamed = await fsPromise.rename(filePath, newFilePath);
+      if (renamed === undefined) {
+        console.log("File was renamed successfully");
+      } else {
+        throw new Error("Error in renaming the file");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const addContent = async (filePath, content) => {
+    let fileHandler;
+
+    try {
+      const buff = Buffer.from(content, "utf-8");
+      fileHandler = await fsPromise.open(filePath, "w");
+      await fileHandler.write(buff, 0, buff.byteLength, 0); // the buffer to write, offset, length, position
+      console.log(`The content is written to the ${filePath}`);
+    } catch (e) {
+      console.log(e.message);
+    } finally {
+      fileHandler.close();
+    }
+  };
 
   // Open the file to read data from it
   const commandFileHandler = await fsPromise.open("./command.txt", "r");
@@ -42,7 +88,7 @@ const fsPromise = require("fs/promises");
     // Decoding the data
     const command = buff.toString("utf-8");
 
-    // Checking the command
+    // Creates a file <path>
     if (command.includes(CREATE_FILE)) {
       // Gets the path from the command
       const filePath = command.substring(CREATE_FILE.length + 1);
@@ -50,11 +96,34 @@ const fsPromise = require("fs/promises");
       // Calling createFile function
       await createFile(filePath);
     }
+
+    // Delete the file <path>
+    if (command.includes(DELETE_FILE)) {
+      const filePath = command.substring(DELETE_FILE.length + 1);
+      deleteFile(filePath);
+    }
+
+    // Rename the file <path> to <path>
+    if (command.includes(RENAME_FILE)) {
+      const _idx = command.indexOf(" to ");
+      const filePath = command.substring(RENAME_FILE.length + 1, _idx);
+      const newFilePath = command.substring(_idx + 4);
+
+      renameFile(filePath, newFilePath);
+    }
+
+    // Add content to the file <path> content: <content>
+    if (command.includes(ADD_CONTENT)) {
+      const _idx = command.indexOf(" content: ");
+      const filePath = command.substring(ADD_CONTENT.length + 1, _idx);
+      const content = command.substring(_idx + 10);
+
+      addContent(filePath, content);
+    }
   });
 
   // Watch the file "command.txt"
   const watcher = fsPromise.watch("./command.txt");
-
   for await (const event of watcher) {
     if (event.eventType === "change") {
       // emitting change event
